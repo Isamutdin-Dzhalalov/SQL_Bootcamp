f7e57cbb174975099eeeae35f090321e8ef188cd
Isamutdin <isamutdin92@gmail.com>
2024-12-09 20:23:31 +0300

    added all solution

--- day01/day01_ex00.sql
+++ day01/day01_ex00.sql
@@ -1,7 +1,5 @@
-SELECT object_id, object_name FROM (
 SELECT id AS object_id, pizza_name AS object_name FROM menu
 UNION ALL
-SELECT id AS object_id, name AS object_name FROM person)
-AS combined_table
+SELECT id AS object_id, name AS object_name FROM person
 ORDER BY object_id, object_name;
 
